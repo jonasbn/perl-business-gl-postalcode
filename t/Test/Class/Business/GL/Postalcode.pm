d1f02ece08bf73958beec7996ee385a4a1641a71
jonasbn <jonasbn@logicLAB.dk>
2014-08-04 17:09:56 +0200

    More tests (Test::Class) added

---
+++ t/Test/Class/Business/GL/Postalcode.pm
@@ -0,0 +1,49 @@
+package Test::Class::Business::GL::Postalcode;
+
+# $Id$
+
+use strict;
+use warnings;
+use base qw(Test::Class);
+use Test::More;
+use Tree::Simple;
+use Test::Exception;
+use Env qw($TEST_VERBOSE);
+
+sub startup : Test(startup => 1) {
+    my $self = shift;
+
+    use_ok( 'Business::GL::Postalcode', qw(validate_postalcode get_all_postalcodes get_all_data) );
+};
+
+sub test_get_all_postalcodes : Test(2) {
+    ok(my $postalcodes_ref = get_all_postalcodes(), 'calling get all postalcodes');
+    is(scalar(@{$postalcodes_ref}), 33), 'asserting number of postalcodes';
+}
+
+sub test_get_all_data : Test(2) {
+    ok(my $postalcodes_ref = get_all_data(), 'calling get_all_data');
+
+    is(scalar(@{$postalcodes_ref}), 33, 'asserting number of postalcodes');
+}
+
+sub test_validate : Test(5) {
+    my $self = shift;
+
+    my @invalids = qw();
+    my @valids = qw();
+
+    foreach (1 .. 9999) {
+        my $number = sprintf '%04d', $_;
+        if (not validate_postalcode($number)) {
+            push @invalids, $number;
+        } else {
+            push @valids, $number;
+        }
+    }
+
+    is(scalar @invalids, 9967);
+    is(scalar @valids, 33);
+}
+
+1;
---
+++ t/runtests.t
@@ -0,0 +1,14 @@
+#!/usr/local/bin/perl -T
+
+# $Id$
+
+# $HeadURL$
+
+use strict;
+use warnings;
+
+use lib qw(t);
+
+use Test::Class::Business::GL::Postalcode;
+
+Test::Class->runtests;
